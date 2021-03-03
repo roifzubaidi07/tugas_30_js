c68b250912af1df73632021e8c4f7a041295e9ca
M. Ro'if <roifzubaidi5@gmail.com>
2021-03-04 04:10:39 +0700

    pengerjaan tugas 30 JS NIOMIC

---
+++ tugas30.js
@@ -0,0 +1,6 @@
+function regex(value) {
+  const ambilData = value.match(/[e,s]/g);
+  console.log(ambilData);
+}
+
+regex('Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat');
